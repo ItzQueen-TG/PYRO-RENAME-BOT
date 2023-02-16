d64909b27d066619a87f9a323549bf28b1c324ca
Mуѕтєяισυѕ gιяℓ <102360292+ItzQueen-TG@users.noreply.github.com>
2023-02-16 19:40:04 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -1,41 +1,36 @@
-from helper.utils import progress_for_pyrogram, convert
-from pyrogram import Client, filters
-from pyrogram.types import (  InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
-from helper.database import db
+import re
 import os 
-import humanize
-from PIL import Image
+import math
 import time
+import humanize
+import asyncio
 
-@Client.on_callback_query(filters.regex('cancel'))
-async def cancel(bot,update):
-	try:
-           await update.message.delete()
-	except:
-           return
+from pyrogram import Client, filters
+from pyrogram.types import *
+from pyrogram.errors import FloodWait
 
-@Client.on_callback_query(filters.regex('rename'))
-async def rename(bot,update):
-	user_id = update.message.chat.id
-	date = update.message.date
-	await update.message.delete()
-	await update.message.reply_text("__𝙿𝚕𝚎𝚊𝚜𝚎 𝙴𝚗𝚝𝚎𝚛 𝙽𝚎𝚠 𝙵𝚒𝚕𝚎𝙽𝚊𝚖𝚎...__",	
-	reply_to_message_id=update.message.reply_to_message.id,  
-	reply_markup=ForceReply(True))
-	
-@Client.on_callback_query(filters.regex("upload"))
-async def doc(bot,update):
-     type = update.data.split("_")[1]
-     new_name = update.message.text
-     new_filename = new_name.split(":-")[1]
-     file_path = f"downloads/{new_filename}"
-     file = update.message.reply_to_message
-     ms = await update.message.edit("𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳...")
+CAPTION = os.environ.get('CAPTION')
+T_CHANNEL = int(os.environ.get("T_CHANNEL", "-1001837941527"))
+F_CHANNEL = int(os.environ.get("F_CHANNEL", "-1001737494519"))
+FF_CHANNEL = int(os.environ.get("FF_CHANNEL", "-1001661692511"))
+PROGRESS_BAR = "\n\n📁 : {b} | {c}\n🚀 : {a}%\n⚡ : {d}/s\n⏱️ : {f}"
+
+class temp(object):
+    THUMBNAIL = environ.get("THUMBNAIL", "AgACAgUAAxkBAAL6aGPXFgiIpqBsaYT9FAP0K9M9zYl2AAKHszEb-x64VtN5kRGuhsBOAAgBAAMCAAN4AAceBA")
+
+
+@Client.on_message(filters.chat(F_CHANNEL) & (filters.document | filters.video))
+async def doc(bot, msg):
+     media = msg.document or msg.audio or msg.video
+     og_media = getattr(msg, msg.media.value)
+     filename = og_media.file_name
+     new_name = filename
+     file = msg.document or msg.video
+     file_path = f"downloads/{new_name}"
+     sts = await bot.send_message(chat_id=T_CHANNEL, text=f"Trying to Download 📩\n\n`{new_name}`")
      c_time = time.time()
      try:
-     	path = await bot.download_media(message = file, progress=progress_for_pyrogram,progress_args=( "𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳....",  ms, c_time   ))
+     	path = await bot.download_media(message = file, progress=progress_message, progress_args=(f"`{new_name}`", sts, c_time))
      except Exception as e:
      	await ms.edit(e)
      	return 
@@ -43,71 +38,84 @@ async def doc(bot,update):
      dow_file_name = splitpath[1]
      old_file_name =f"downloads/{dow_file_name}"
      os.rename(old_file_name,file_path)
-     duration = 0
-     try:
-        metadata = extractMetadata(createParser(file_path))
-        if metadata.has("duration"):
-           duration = metadata.get('duration').seconds
-     except:
-        pass
-     user_id = int(update.message.chat.id) 
-     ph_path = None 
      media = getattr(file, file.media.value)
-     c_caption = await db.get_caption(update.message.chat.id)
-     c_thumb = await db.get_thumbnail(update.message.chat.id)
-     if c_caption:
+     if CAPTION:
          try:
              caption = c_caption.format(filename=new_filename, filesize=humanize.naturalsize(media.file_size), duration=convert(duration))
          except Exception as e:
-             await ms.edit(text=f"Your caption Error unexpected keyword ●> ({e})")
+             await sts.edit(text=f"Your caption Error unexpected keyword ●> ({e})")
              return 
      else:
-         caption = f"**{new_filename}**"
-     if (media.thumbs or c_thumb):
-         if c_thumb:
-            ph_path = await bot.download_media(c_thumb) 
-         else:
-            ph_path = await bot.download_media(media.thumbs[0].file_id)
-         Image.open(ph_path).convert("RGB").save(ph_path)
-         img = Image.open(ph_path)
-         img.resize((320, 320))
-         img.save(ph_path, "JPEG")
-     await ms.edit("𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝚄𝙿𝙻𝙾𝙰𝙳𝙸𝙽𝙶....")
-     c_time = time.time() 
+         caption = f"`{new_name}`"
+     raw_thumbnail = temp.THUMBNAIL 
+     if raw_thumbnail:
+        og_thumbnail = await bot.download_media(raw_thumbnail)
+     else:
+         og_thumbnail = await bot.download_media(og_media.thumbs[0].file_id)
+     await sts.edit(f"Trying to Uploading\n`{new_name}`")
+     c_time = time.time()
      try:
-        if type == "document":
-           await bot.send_document(
-		    update.message.chat.id,
-                    document=file_path,
-                    thumb=ph_path, 
-                    caption=caption, 
-                    progress=progress_for_pyrogram,
-                    progress_args=( "𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝚄𝙿𝙻𝙾𝙰𝙳𝙸𝙽𝙶....",  ms, c_time   ))
-        elif type == "video": 
-            await bot.send_video(
-		    update.message.chat.id,
-		    video=file_path,
-		    caption=caption,
-		    thumb=ph_path,
-		    duration=duration,
-		    progress=progress_for_pyrogram,
-		    progress_args=( "𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝚄𝙿𝙻𝙾𝙰𝙳𝙸𝙽𝙶....",  ms, c_time))
-        elif type == "audio": 
-            await bot.send_audio(
-		    update.message.chat.id,
-		    audio=file_path,
-		    caption=caption,
-		    thumb=ph_path,
-		    duration=duration,
-		    progress=progress_for_pyrogram,
-		    progress_args=( "𝚃𝚁𝚈𝙸𝙽𝙶 𝚃𝙾 𝚄𝙿𝙻𝙾𝙰𝙳𝙸𝙽𝙶....",  ms, c_time   )) 
-     except Exception as e: 
-         await ms.edit(f" Erro {e}") 
-         os.remove(file_path)
-         if ph_path:
-           os.remove(ph_path)
-         return 
-     await ms.delete() 
-     os.remove(file_path) 
-     if ph_path:
-        os.remove(ph_path) 
+         await bot.send_document(
+                   T_CHANNEL, 
+                   document=file_path,
+                   thumb=og_thumbnail, 
+                   caption=cap,
+                   progress=progress_message, 
+                   progress_args=(f"Uploading 📤\n\n`{new_name}`", sts, c_time))
+    except Exception as e:  
+        await msg.copy(chat_id=T_CHANNEL, caption = cap)
+        await sts.delete()
+        return               
+    try:
+        os.remove(file_path)
+        os.remove(og_thumbnail)
+    except:
+        pass
+    await sts.delete()
+
+
+async def progress_message(current, total, ud_type, message, start):
+    now = time.time()
+    diff = now - start
+    if round(diff % 6.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        elapsed_time = TimeFormatter(milliseconds=elapsed_time)
+        estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)                                    
+        progress = "\n{0}{1}".format(
+            ''.join(["⬢" for i in range(math.floor(percentage / 5))]),
+            ''.join(["⬡" for i in range(20 - math.floor(percentage / 5))]))                                  
+        tmp = progress + PROGRESS_BAR.format(
+            a=round(percentage, 2),
+            b=humanbytes(current),
+            c=humanbytes(total),
+            d=humanbytes(speed),
+            f=estimated_total_time if estimated_total_time != '' else "0 s")                               
+        try:
+            await message.edit(text="{}\n{}".format(ud_type, tmp))         
+        except:
+            pass
+
+def humanbytes(size):
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units):
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
+
+def TimeFormatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = ((str(days) + "d, ") if days else "") + \
+          ((str(hours) + "h, ") if hours else "") + \
+          ((str(minutes) + "m, ") if minutes else "") + \
+          ((str(seconds) + "s, ") if seconds else "") + \
+          ((str(milliseconds) + "ms, ") if milliseconds else "")
+    return tmp[:-2]
