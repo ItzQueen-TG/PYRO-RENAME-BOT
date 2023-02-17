48594eec866a88dd7885b2b90a52bbecd1aa004f
Mуѕтєяισυѕ gιяℓ <102360292+ItzQueen-TG@users.noreply.github.com>
2023-02-17 06:26:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,17 +8,9 @@ API_HASH = os.environ.get("API_HASH", "")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "") 
+STRING = os.environ.get("STRING", "")
 
-DB_NAME = os.environ.get("DB_NAME","")     
-
-DB_URL = os.environ.get("DB_URL","")
- 
-FLOOD = int(os.environ.get("FLOOD", "10"))
-
-START_PIC = os.environ.get("START_PIC", "")
-
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
 PORT = os.environ.get('PORT', '8080')
 
